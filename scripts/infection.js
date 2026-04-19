3968e104fcda10fa857f47d8dd73ebdfaa162104
Morphic <osorezetsumei@googlemail.com>
2026-04-19 18:09:41 +0100

    Updated the script for breathing infection

--- scripts/infection.js
+++ scripts/infection.js
@@ -29,14 +29,26 @@ if(document.body.classList.contains("infection-enabled")){
     }
 
 function draw() {
-console.log("DRAWING");
+
+  let pulseSpeed = 1000;
+  let nextSpeedChange = Date.now() + 2000;
+
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // redish background
-  //ctx.fillStyle = `rgba(0, 255, 0, ${greenAlpha})`;
   //This should make the canvas breathe
-  let pulse = 0.1 + Math.sin(Date.now() /1000) * 0.1;
-  ctx.fillStyle = `rgba(200, 80, 0, ${pulse})`;//change green back to 50 if 80 is too much
+ /* let pulse = 0.1 + Math.sin(Date.now() /1000) * 0.1;
+  ctx.fillStyle = `rgba(200, 80, 0, ${pulse})`;//change green back to 50 if 80 is too much*/
+
+  //random breathing
+  if(Date.now() > nextSpeedChange){
+       pulseSpeed = 500 + Math.random() * 2000;
+       nextSpeedChange = Date.now() + 2000 + Math.random() * 2000;
+  }
+
+  let pulse = 0.1 + Math.sin(Date.now() / pulseSpeed) * 0.1;
+  ctx.fillStyle = `rgba(200, 50, 0, ${pulse})`;
+
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // particles
